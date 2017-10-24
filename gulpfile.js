ea2377113ddd553590fec09d6d5fc787f530339a
IvelinaCh <lynnln91@gmail.com>
2017-10-24 14:29:12 +0300

    webp

--- gulpfile.js
+++ gulpfile.js
@@ -99,3 +99,9 @@ gulp.task("copy", function () {
 gulp.task("clean", function () {
   return del("build");
 });
+
+gulp.task("webp", function () {
+  return gulp.src("img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
